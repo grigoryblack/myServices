503b718c557946bed0e5bd60fa9454e135463ba4
gd <gd@callibri.ru>
2025-09-17 17:21:15 +0500

    Фикс темы

--- src/app/viewport.ts
+++ src/app/viewport.ts
@@ -1,7 +1,10 @@
 import type { Viewport } from 'next'
 
 export const viewport: Viewport = {
-  themeColor: '#3b82f6',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#0f172a' }
+  ],
   width: 'device-width',
   initialScale: 1,
   maximumScale: 5,
